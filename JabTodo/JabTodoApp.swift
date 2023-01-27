57ed80b5f9038c7aa6ef8a7fe942536a64ea4403
Julio Figueiredo <juliosfigueiredo@gmail.com>
2023-01-26 23:49:45 -0300

    Método para o app checar se o usuário clicou no botão Começar na tela de boas vinda para que a mesma não apareça novamente na próxima vez que o app for aberto.

--- JabTodo/JabTodoApp.swift
+++ JabTodo/JabTodoApp.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 @main
 struct JabTodoApp: App {
+    var viewModel: JobViewModel
+    
+    // MARK: - Checar se o usuario já clicou no botão Começar quando o app é reiniciado novamente
+    init() {
+        viewModel = JobViewModel()
+        let appInit = viewModel.checkAppInit()
+        if appInit {
+            viewModel.appInit = false
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
-            JobView(viewModel: JobViewModel())
+            JobView(viewModel: viewModel)
         }
     }
 }
